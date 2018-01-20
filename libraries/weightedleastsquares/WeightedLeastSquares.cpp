718c52cbce7141ccc8cafc1b536499af5de41a25
Paul Wesson <paul@wesson.net>
2018-01-20 18:14:11 +0000

    Added machine learning least squares

---
+++ libraries/weightedleastsquares/WeightedLeastSquares.cpp
@@ -0,0 +1,101 @@
+#include "WeightedLeastSquares.h"
+
+WeightedLeastSquares::WeightedLeastSquares(){
+
+   // Initialise
+   Reset();
+
+}
+
+void WeightedLeastSquares::Reset(){
+
+   discount= 0.98;
+
+   staticRegression = false;
+   w = 1;
+   slope = 0;
+   intercept = 0;
+   sum_w = 0;
+   sum_wx = 0;
+   sum_wy = 0;
+   sum_wxx = 0;
+   sum_wxy = 0;
+}
+
+void WeightedLeastSquares::SetRegression(float setslope, float setintercept)
+{
+   // Set the regression coefficient
+   slope = setslope;
+   intercept = setintercept;
+   staticRegression = true;
+}
+
+void WeightedLeastSquares::SetDiscount(float d){
+
+   // The discount rate of old observations
+   discount = d;
+}
+
+float WeightedLeastSquares::PredictY(float x){
+
+   // Predict using the regression line
+   return slope * x + intercept;
+}
+
+float WeightedLeastSquares::PredictX(float y){
+
+   // Predict using the regression line
+   if (slope == 0) return 0;
+
+   return (y - intercept) / slope;
+}
+
+float WeightedLeastSquares::PredictXlimit(float y, float miny, float maxy){
+
+   // Predict using the regression line
+   if (slope == 0) return 0;
+
+   double predicty = (y - intercept) / slope;
+
+   if (predicty > maxy) return maxy;
+   if (predicty < miny) return miny;
+
+   return (y - intercept) / slope;
+}
+
+void WeightedLeastSquares::AddReading(float x, float y)
+{
+   //Is the regression static. We want it fixed
+
+   if (staticRegression) return;
+
+   //First discount all the old observations
+
+   sum_w =   discount * sum_w;
+   sum_wx =  discount * sum_wx;
+   sum_wy =  discount * sum_wy;
+   sum_wxx = discount * sum_wxx;
+   sum_wxy = discount * sum_wxy;
+
+   //Now add in the new observation
+
+   sum_w =   sum_w + w;
+   sum_wx =  sum_wx + w * x;
+   sum_wy =  sum_wy + w * y;
+   sum_wxx = sum_wxx + w * x * x;
+   sum_wxy = sum_wxy + w * x * y;
+
+   //Determinate of the matrix
+
+   determinate = (sum_w * sum_wxx) - (sum_wx * sum_wx);
+
+   if (determinate != 0)
+   {
+      //Update the regression intercept
+      intercept = (sum_wy * sum_wxx - sum_wx * sum_wxy) / determinate;
+   
+      //Update the regression slope
+      slope = (sum_w * sum_wxy - sum_wx * sum_wy) / determinate;
+   }
+
+}
