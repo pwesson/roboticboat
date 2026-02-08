1931e816de3c397d1ff633b6e7ae81e7bf94ab18
Paul Wesson <paulw@wesson.net>
2026-02-08 16:50:24 +0000

    Updated rudder logic

--- Rudder.cpp
+++ Rudder.cpp
@@ -1,6 +1,7 @@
-// Teensy 4.0 with Kyosho Seawind
-// Copyright (C) 2020 https://www.roboticboat.uk
-// 5925b03d-41c7-4051-b890-02b7775383dd
+// Rudder logic
+// Copyright (C) 2026
+// af6de076-2c5e-4a1e-ae92-1622619e380c
+// Refined in collaboration with Microsoft Copilot
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,250 +18,200 @@
 // These Terms shall be governed and construed in accordance with the laws of 
 // England and Wales, without regard to its conflict of law provisions.
 
-
-
 #include "Rudder.h"
+#include <Arduino.h>
 #include <math.h>
-#include <stdlib.h>
-#include "Arduino.h"
 
-// Converts degrees to radians.
-//#define radians(angleDegrees) (angleDegrees * M_PI / 180.0)
- 
-// Converts radians to degrees.
-//#define degrees(angleRadians) (angleRadians * 180.0 / M_PI)
-
-Rudder::Rudder(){
-
-   // Initialise the Rudder object
-
-  maxposition = 180;
-  minposition = 0;
+//--------------------------------------------------------------
+// Constructor
+//--------------------------------------------------------------
+Rudder::Rudder() {
+    minposition = 0;
+    maxposition = 180;
 }
 
+//--------------------------------------------------------------
+// GyroUpdateMean()
+// Updates the mean gyro value when the boat is stationary.
+//--------------------------------------------------------------
 void Rudder::GyroUpdateMean(float bearing, float gyroznew)
 {
-  // We have received a new bearing. Is it different from the old bearing
+    // If bearing hasn't changed, boat may be stationary
+    if (bearing == gyrooldbearing) {
+        gyroNbearing++;
 
-  if (bearing == gyrooldbearing)
-  {
-    // Increase the static bearing counter
-    gyroNbearing++;
-
-    // Is the boat stationary? If so can calibrate the gyro
-    // Two is an arbitary number at the moment
-    if (gyroNbearing > 2)
-    {
-      // The boat is considered stationary
-      gyrozmean = gyrolambda * gyroznew + (1-gyrolambda) * gyrozmean;
+        // After N stable readings, update mean gyro drift
+        if (gyroNbearing > 2) {
+            gyrozmean = gyrolambda * gyroznew + (1 - gyrolambda) * gyrozmean;
+        }
+    } else {
+        gyroNbearing = 0;
     }
-  }
-  else
-  {
-    // Reset the static bearing counter
-    gyroNbearing = 0;
-  }
 
-  // Remember the old bearing
-  gyrooldbearing = bearing;
+    gyrooldbearing = bearing;
 }
 
+//--------------------------------------------------------------
+// AddGyroReading()
+// Adds a gyro reading only when rudder is within valid range.
+//--------------------------------------------------------------
 void Rudder::AddGyroReading(int rudder, float gyroznew)
 {
-  // Add new reading of gyro is rudder is within a range
-  if (rudder >= minposition && rudder <=maxposition) 
-  {
-    // Want to add the rotation rate that have been calibrated
-    gyro.AddReading(rudder, gyroznew - gyrozmean);
-  }
+    if (rudder >= minposition && rudder <= maxposition) {
+        gyro.AddReading(rudder, gyroznew - gyrozmean);
+    }
 }
 
-void Rudder::SetGyroRegressionStatic(float setslope, float setintercept)
+//--------------------------------------------------------------
+// SetGyroRegressionStatic()
+// Manually sets slope/intercept for gyro regression.
+//--------------------------------------------------------------
+void Rudder::SetGyroRegressionStatic(float slope, float intercept)
 {
-  // Set the gyro regression parameters
-  gyro.SetRegression(setslope, setintercept);
+    gyro.SetRegression(slope, intercept);
 }
 
+//--------------------------------------------------------------
+// AddReading()
+// Adds bearing-change reading and updates rudder moving average.
+//--------------------------------------------------------------
 void Rudder::AddReading(unsigned long mytime, int rudder, float bearingchanged)
 {
-  // Add new reading of bearing change over 1 second 
-  if (rudder >=minposition && rudder <=maxposition) wls.AddReading(rudder, bearingchanged);
+    if (rudder >= minposition && rudder <= maxposition)
+        wls.AddReading(rudder, bearingchanged);
 
-  // Update the moving average of the rudder position
-  rudderma = lambda * rudder + (1 - lambda) * rudderma;
+    // Update moving average of rudder position
+    rudderma = lambda * rudder + (1 - lambda) * rudderma;
 }
 
+//--------------------------------------------------------------
+// ClearLine()
+// Resets all line-following waypoints.
+//--------------------------------------------------------------
 void Rudder::ClearLine()
 {
-  //Clear the waypoint line A and B
-  waypointA.latitude = 0;
-  waypointA.longitude = 0;
-    
-  waypointB.latitude = 0;
-  waypointB.longitude = 0;
-
-  alongtrack = 0;
-  crosstrack = 0;
+    waypointA = {0,0};
+    waypointB = {0,0};
+    alongtrack = 0;
+    crosstrack = 0;
 }
 
-
+//--------------------------------------------------------------
+// SetSensitivity()
+// Controls how aggressively the rudder responds.
+//--------------------------------------------------------------
 void Rudder::SetSensitivity(int val)
 {
-   sensitivity = val;
+    sensitivity = val;
 }
 
+//--------------------------------------------------------------
+// NewLine() - version 1
+// Creates a line based on current position + heading.
+//--------------------------------------------------------------
 void Rudder::NewLine(double latitude, double longitude, double bearing)
 {
-  // The Human was in control, so now initialise the Robot task
-  // The Robot wants to know the line AB to follow. 
-      
-  // The current position of the boat
-  waypointX.latitude = latitude;
-  waypointX.longitude = longitude;
+    waypointX = {latitude, longitude};
 
-  // Calculate the waypoint B that is 1000 meters ahead
-  // The current boat heading is bearing (degrees)
-  waypointB = nav.waypoint(waypointX, bearing, 1000);
+    waypointB = nav.waypoint(waypointX, bearing, 1000);
+    waypointA = nav.waypoint(waypointX, nav.clean(bearing - 180), 100);
 
-  // Calculate the waypoint A which is 100 meters behind
-  // This is required as if the boat slips backwards slightly, it will still be between A and B
-  waypointA = nav.waypoint(waypointX, nav.clean(bearing - 180), 100);
-
-  // Target distance to travel along AB
-  // We start the line already 100m along it.
-  alongtrackobjective = 1100;
-
+    alongtrackobjective = 1100; // 100m behind + 1000m ahead
 }
 
-
+//--------------------------------------------------------------
+// NewLine() - version 2
+// Creates a line based on current position + next waypoint.
+//--------------------------------------------------------------
 void Rudder::NewLine(double latitude, double longitude, double nextlat, double nextlon)
 {
-  // The current position of the boat
-  waypointX.latitude = latitude;
-  waypointX.longitude = longitude;
+    waypointX = {latitude, longitude};
+    waypointF = {nextlat, nextlon};
 
-  // The next waypoint to move towards
-  waypointF.latitude = nextlat;
-  waypointF.longitude = nextlon;
+    double nextbearing = nav.bearing(waypointX, waypointF);
 
-  // What is the bearing from X to the waypoint F?
-  double nextbearing = nav.bearing(waypointX, waypointF);
+    waypointB = nav.waypoint(waypointX, nextbearing, 1000);
+    waypointA = nav.waypoint(waypointX, nav.clean(nextbearing - 180), 100);
 
-  // Calculate the waypoint B that is 1000 meters ahead
-  // The current boat heading is bearing (degrees)
-  waypointB = nav.waypoint(waypointX, nextbearing, 1000);
-
-  // Calculate the waypoint A which is 100 meters behind
-  // This is required as if the boat slips backwards slightly, it will still be between A and B
-  waypointA = nav.waypoint(waypointX, nav.clean(nextbearing - 180), 100);
-
-  // The distance to travel along line AB is AF
-  // We start the line already 100m along it
-  alongtrackobjective = nav.distance(waypointA, waypointF);
-
+    alongtrackobjective = nav.distance(waypointA, waypointF);
 }
 
+//--------------------------------------------------------------
+// Update()
+// Main navigation update: computes target bearing and rudder.
+//--------------------------------------------------------------
 int Rudder::Update(double latitude, double longitude, float bearing)
 {
-   // Where is the boat along the line AB?
-   // WARNING - this logic does assume the GPS location is known and active.
-      
-   // The current position of the boat
-   waypointX.latitude = latitude;
-   waypointX.longitude = longitude;
-      
-   // Distance along the track between A and B
-   alongtrack = nav.alongtrack(waypointA, waypointB, waypointX);
-      
-   // Distance to the track between A and B
-   // Not used in the calculations, but nice to know as its the error
-   crosstrack = nav.crosstrack(waypointA, waypointB, waypointX);
-      
-   // Lat/Lon closest point along the track to the boat
-   waypointD = nav.pointontrack(waypointA, waypointB, waypointX);
-   
-   // What is the bearing of D to the waypoint B?
-   double localbearing = nav.bearing(waypointD, waypointB);
+    waypointX = {latitude, longitude};
 
-   // Create a local waypointE which can be set 10 meters, along the line, from D
-   waypointE = nav.waypoint(waypointD, localbearing , localdistance);
-   
-   // Now calculate bearing from X (current location) to local waypointE
-   targetbearing = nav.clean(nav.bearing(waypointX, waypointE));
+    alongtrack = nav.alongtrack(waypointA, waypointB, waypointX);
+    crosstrack = nav.crosstrack(waypointA, waypointB, waypointX);
+    waypointD = nav.pointontrack(waypointA, waypointB, waypointX);
 
-   // The difference between the boat heading and the targetbearing
-   requiredBearingChange = targetbearing - bearing;
+    double localbearing = nav.bearing(waypointD, waypointB);
+    waypointE = nav.waypoint(waypointD, localbearing, localdistance);
 
-   // Ensure the change in bearing is between -180 and 180 degrees
-   if (requiredBearingChange > 180) requiredBearingChange -= 360;
-   if (requiredBearingChange < -180) requiredBearingChange += 360;
+    targetbearing = nav.clean(nav.bearing(waypointX, waypointE));
 
-   // Restrict the maximum change in the bearing, so rudder can keep up
-   targetbearing = nav.clean(bearing + requiredBearingChange);
+    requiredBearingChange = targetbearing - bearing;
 
-   //Restrict the rudder position (within to be between min and max position of servo)
-   //Halve the degree error by 2, so hopefully get on target in 2 seconds.
-   
-   // Negative sign as gyro has positive change as bearing reduce
-   return (int)gyro.PredictXlimit(-requiredBearingChange/sensitivity, minposition , maxposition);
+    // Wrap to [-180, 180]
+    if (requiredBearingChange > 180) requiredBearingChange -= 360;
+    if (requiredBearingChange < -180) requiredBearingChange += 360;
 
+    targetbearing = nav.clean(bearing + requiredBearingChange);
+
+    return (int)gyro.PredictXlimit(-requiredBearingChange / sensitivity,
+                                   minposition, maxposition);
 }
 
+//--------------------------------------------------------------
+// ChooseTarget()
+// Chooses port or starboard tack based on wind direction.
+//--------------------------------------------------------------
 float Rudder::ChooseTarget(float windDirection, float bearing)
 {
-   // Want to find the tack bearing to be on
-   // windDirection +/- 60 degrees say
-  
-   float windPort = windDirection + 60;
-   if (windPort > 360) {windPort -= 360;}
+    float windPort = fmod(windDirection + 60 + 360, 360);
+    float windStar = fmod(windDirection - 60 + 360, 360);
 
-   float windStar = windDirection - 60;
-   if (windStar < 0) {windStar += 360;}
+    portBearingChange = windPort - bearing;
+    starBearingChange = windStar - bearing;
 
-   // The difference between the boat heading and the targetbearing
-   portBearingChange  = windPort - bearing;
-   starBearingChange  = windStar - bearing;
+    // Wrap both to [-180, 180]
+    if (portBearingChange > 180) portBearingChange -= 360;
+    if (portBearingChange < -180) portBearingChange += 360;
 
-   // Ensure the change in bearing is between -180 and 180 degrees
-   if (portBearingChange > 180) portBearingChange -= 360;
-   if (portBearingChange < -180) portBearingChange += 360;
+    if (starBearingChange > 180) starBearingChange -= 360;
+    if (starBearingChange < -180) starBearingChange += 360;
 
-   // Ensure the change in bearing is between -180 and 180 degrees
-   if (starBearingChange > 180) starBearingChange -= 360;
-   if (starBearingChange < -180) starBearingChange += 360;
-
-   // Choose the bearing that is closest
-   if (abs(portBearingChange) < abs(starBearingChange))
-   {
-      // Have chosen the Port tack
-      return windPort;
-   }
-
-  // Have chosen the Starboard tack
-  return windStar;
+    return (abs(portBearingChange) < abs(starBearingChange))
+           ? windPort
+           : windStar;
 }
 
+//--------------------------------------------------------------
+// UpdateTarget()
+// Steers toward a specific bearing.
+//--------------------------------------------------------------
 int Rudder::UpdateTarget(float target, float bearing)
 {
-   // The difference between the boat heading and the targetbearing
-   requiredBearingChange = target - bearing;
+    requiredBearingChange = target - bearing;
 
-   // Ensure the change in bearing is between -180 and 180 degrees
-   if (requiredBearingChange > 180) requiredBearingChange -= 360;
-   if (requiredBearingChange < -180) requiredBearingChange += 360;
+    if (requiredBearingChange > 180) requiredBearingChange -= 360;
+    if (requiredBearingChange < -180) requiredBearingChange += 360;
 
-   // Restrict the maximum change in the bearing, so rudder can keep up
-   targetbearing = nav.clean(bearing + requiredBearingChange);
+    targetbearing = nav.clean(bearing + requiredBearingChange);
 
-   //Restrict the rudder position (within to be between min and max position of servo)
-   //Halve the degree error by 2, so hopefully get on target in 2 seconds.
-   
-   // Negative sign as gyro has positive change as bearing reduce
-   return (int)gyro.PredictXlimit(-requiredBearingChange/sensitivity, minposition , maxposition);
+    return (int)gyro.PredictXlimit(-requiredBearingChange / sensitivity,
+                                   minposition, maxposition);
 }
 
+//--------------------------------------------------------------
+// Update() - simple version
+// Applies gyro regression to a known degree change.
+//--------------------------------------------------------------
 int Rudder::Update(float degreechange)
 {
-   // This should compensate for some weather helm
-   return (int)gyro.PredictXlimit(-degreechange/sensitivity, minposition , maxposition);
+    return (int)gyro.PredictXlimit(-degreechange / sensitivity,
+                                   minposition, maxposition);
 }
--- Rudder.h
+++ Rudder.h
@@ -1,71 +1,104 @@
-// Licensed under the GNU General Public License v3.0
+// Rudder logic
+// Copyright (C) 2026
+// 45886ce6-fdb4-47fa-a79c-d34a9543a321
+// Refined in collaboration with Microsoft Copilot
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// These Terms shall be governed and construed in accordance with the laws of 
+// England and Wales, without regard to its conflict of law provisions.
+
+
+#ifndef RUDDER_H
+#define RUDDER_H
 
 #include <Navigation.h>
 #include <WeightedLeastSquares.h>
 
-#ifndef Rudder_h
-  #define Rudder_h
+//--------------------------------------------------------------
+// Rudder Class
+// Handles:
+//  - Line-following navigation (waypoints A → B)
+//  - Rudder control based on bearing error
+//  - Gyro calibration and regression
+//  - Moving averages and smoothing
+//--------------------------------------------------------------
 
-  class Rudder{
-	public:
-		Rudder();
+class Rudder {
 
-		void NewLine(double, double, double);
-		void NewLine(double, double, double, double);
-		void ClearLine();
-		void AddReading(unsigned long, int, float);
-		void GyroUpdateMean(float, float);
-		void AddGyroReading(int, float);
-		void SetGyroRegressionStatic(float, float);
-		void SetSensitivity(int);
-		
-		int Update(double, double, float);
-                int Update(float);
-		int UpdateTarget(float, float);
-		float ChooseTarget(float, float);
-		
-		Navigation nav;
-		WeightedLeastSquares wls;
-		WeightedLeastSquares gyro;
+public:
+    Rudder();
 
-		int minposition;
-		int maxposition;
+    // --- Line creation and clearing ---
+    void NewLine(double lat, double lon, double bearingDeg);
+    void NewLine(double lat, double lon, double nextLat, double nextLon);
+    void ClearLine();
 
-		coordinate waypointX;
-		coordinate waypointA;
-		coordinate waypointB;
-		coordinate waypointD;
-		coordinate waypointE;
-		coordinate waypointF;
+    // --- Data input ---
+    void AddReading(unsigned long t, int rudderPos, float bearingChange);
+    void AddGyroReading(int rudderPos, float gyroz);
+    void GyroUpdateMean(float bearing, float gyroz);
+    void SetGyroRegressionStatic(float slope, float intercept);
+    void SetSensitivity(int val);
 
-		float reversebearing = 0;
-		float alongtrack = 0;
-		float alongtrackobjective = 0;
-		float crosstrack = 0;
-		float localdistance = 10;
-		
-		float bearing = 0;
-		float oldbearing = 0;
-		float bearingchanged = 0;
-		float requiredBearingChange = 0;
-		float targetbearing = 0;
-		float portBearingChange = 0;
-		float starBearingChange = 0;
+    // --- Rudder control ---
+    int Update(double lat, double lon, float bearingDeg);
+    int Update(float degreeChange);
+    int UpdateTarget(float targetBearing, float currentBearing);
+    float ChooseTarget(float windDirection, float currentBearing);
 
-		float rudderma = 0;
-		float lambda = 0.1;
-		float sensitivity = 2;
-		
-                int gyroNbearing = 0;
-		float gyrolambda = 0.01;
-		float gyrooldbearing = 0;
-		float gyrozmean = 0;
+    // --- Public navigation helpers ---
+    Navigation nav;
+    WeightedLeastSquares wls;   // Bearing regression
+    WeightedLeastSquares gyro;  // Gyro regression
 
-		unsigned long atime = 0;
-		unsigned long btime = 0;
-	        
-	private:
-		        		
-  };
+    // Rudder limits
+    int minposition = 0;
+    int maxposition = 180;
+
+    // Waypoints
+    coordinate waypointX;
+    coordinate waypointA;
+    coordinate waypointB;
+    coordinate waypointD;
+    coordinate waypointE;
+    coordinate waypointF;
+
+    // Tracking variables
+    float alongtrack = 0;
+    float alongtrackobjective = 0;
+    float crosstrack = 0;
+    float localdistance = 10;
+
+    // Bearing-related values
+    float requiredBearingChange = 0;
+    float targetbearing = 0;
+    float portBearingChange = 0;
+    float starBearingChange = 0;
+
+    // Rudder smoothing
+    float rudderma = 0;
+    float lambda = 0.1;
+    float sensitivity = 2;
+
+    // Gyro calibration
+    int   gyroNbearing = 0;
+    float gyrolambda = 0.01;
+    float gyrooldbearing = 0;
+    float gyrozmean = 0;
+
+private:
+
+};
 
 #endif
